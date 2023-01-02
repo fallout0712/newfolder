b7dfbd0e521e1d7e2a5485f07ea5caaa646a4a3d
ilim <ilimdar.tada10@gmail.com>
2023-01-02 14:49:41 +0300

    first web commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log (2 + 2 * 2 !=8);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;		
+console.log (personalMovieDB);
