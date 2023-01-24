002f159e978beba626e8ccaecf57883a7cfd3b21
ilim <ilimdar.tada10@gmail.com>
2023-01-24 18:38:27 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 "use strict";
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start (){
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)){
+			numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		}
+}
+start(); 
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -12,7 +20,7 @@ const personalMovieDB = {
 for (let i = 0; i<2; i++){
 	const a = prompt('Один из последних просмотренных фильмов?', ''),
 			b = prompt('На сколько оцените его?', '');
-		if (a != null && b != null && a !='' && b != '' && a.length <50) {
+		if (a != null && b != null && a !='' && b != '' && a.length < 50) {
 			personalMovieDB.movies[a] = b;
 			console.log('done');
 		} else {
@@ -32,3 +40,9 @@ if (personalMovieDB.count < 10){
 }
 
 console.log(personalMovieDB);
+
+
+
+
+
+
