631c73bd84afe455093ea2938f8ea2d78872c194
ilim <ilimdar.tada10@gmail.com>
2023-01-25 17:39:24 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -17,27 +17,33 @@ const personalMovieDB = {
 	privat: false
 };
 
-for (let i = 0; i<2; i++){
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', '');
-		if (a != null && b != null && a !='' && b != '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('error');
-			i--;
-		}
+function rememberMyFilms(){
+	for (let i = 0; i<2; i++){
+		const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+			if (a != null && b != null && a !='' && b != '' && a.length < 50) {
+				personalMovieDB.movies[a] = b;
+				console.log('done');
+			} else {
+				console.log('error');
+				i--;
+			}
+	}
 }
+rememberMyFilms();
 
-if (personalMovieDB.count < 10){
-	console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
-	console.log("Вы классический зритель");	
-} else if (personalMovieDB.count >30) {
-	console.log("Вы киноман");
-} else {
-	console.log ("Произошла ошибка");
+function detectPersonalLevel(){
+	if (personalMovieDB.count < 10){
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+		console.log("Вы классический зритель");	
+	} else if (personalMovieDB.count >30) {
+		console.log("Вы киноман");
+	} else {
+		console.log ("Произошла ошибка");
+	}
 }
+detectPersonalLevel();
 
 console.log(personalMovieDB);
 
